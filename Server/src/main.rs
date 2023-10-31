33e16f8efe8c26090bf3423a0449ecc746ec46f0
wOL-Lucas <lucasefernandes333@gmail.com>
2023-10-30 21:38:58 -0300

    Inited multithreading implementation:

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -4,6 +4,7 @@ use std::{
     net::{TcpListener, TcpStream},
     thread,
 };
+use std::sync::Arc;
 
 mod routes;
 use routes::Router;
@@ -11,32 +12,14 @@ use routes::Router;
 fn main() {
     let router = Router::new();
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let router_clone: &Router = &router;
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let cloned_router = router.clone(); // Clone the router for the thread
-
         println!("Connection established!");
 
         thread::spawn(move || {
-            handle_client(cloned_router, stream);
+            router_clone.handle(stream);
         });
-    }
-}
-
-fn handle_client(router: Router, mut stream: TcpStream) {
-    let mut request = String::new();
-    let mut reader = BufReader::new(&mut stream);
-
-    if reader.read_to_string(&mut request).is_err() {
-        return;
-    }
-
-    let http_header = request.lines().next().unwrap_or("").trim();
-
-    if router.get_routes().contains_key(http_header) {
-        router.send_response(stream, http_header);
-    } else {
-        router.send_error(stream);
     }
 }
