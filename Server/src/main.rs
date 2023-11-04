16453a1f2541f41219997e5518f98a4d6ea1ed46
wOL-Lucas <lucasefernandes333@gmail.com>
2023-11-04 19:45:18 -0300

    Fix: interaction with routes module

--- Server/src/main.rs
+++ Server/src/main.rs
@@ -1,25 +1,7 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-    thread,
-};
-use std::sync::Arc;
-
 mod routes;
 use routes::Router;
 
 fn main() {
     let router = Router::new();
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let router_clone: &Router = &router;
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("Connection established!");
-
-        thread::spawn(move || {
-            router_clone.handle(stream);
-        });
-    }
+    router.init_server();
 }
